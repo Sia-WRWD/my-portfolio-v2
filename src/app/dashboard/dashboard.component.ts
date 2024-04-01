b635625570a19759663f75b3d5c50a37ba627136
Sia-WRWD <chinojen7@gmail.com>
2024-04-01 22:04:37 +0800

    Added Navigation

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [NavigationComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+
+}
---
+++ src/app/dashboard/profile-content/profile-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-content.component.html',
+  styleUrl: './profile-content.component.scss'
+})
+export class ProfileContentComponent {
+
+}
