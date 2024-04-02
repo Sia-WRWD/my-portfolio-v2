0626b91451c8be65916eed4f3a5820a637a8c338
Sia Chee Zhen <cheezhensia24@gmail.com>
2024-04-02 15:46:21 +0800

    Edited Navigation, Added Background

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NavigationComponent } from './navigation/navigation.component';
+import { ProfileHeaderComponent } from './profile-header/profile-header.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, ProfileHeaderComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  @ViewChild('video') videoElement!: ElementRef;
 
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    console.log(this.videoElement.nativeElement);
+    // Access the video element and play it
+    this.videoElement.nativeElement.play();
+  }
 }
+
+
