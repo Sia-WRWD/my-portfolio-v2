de41b56b3d0aee179f371c1bcc8c61007de4a7a9
Sia Chee Zhen <cheezhensia24@gmail.com>
2024-04-22 18:00:49 +0800

    Added Sample Carousel

--- src/app/dashboard/profile-content/profile-workexp/profile-workexp.component.ts
+++ src/app/dashboard/profile-content/profile-workexp/profile-workexp.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import anime from 'animejs/lib/anime.es.js';
 import { InViewportDirective } from 'ng-in-viewport';
+import { CarouselModule } from 'ngx-acuw';
 
 @Component({
   selector: 'app-profile-workexp',
   standalone: true,
-  imports: [InViewportDirective],
+  imports: [InViewportDirective, CarouselModule],
   templateUrl: './profile-workexp.component.html',
   styleUrl: './profile-workexp.component.scss'
 })
 export class ProfileWorkexpComponent {
   animationPlayed: boolean = false;
+
+  workExpCarouselItems = [
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzYrRIui75mIjWZ8L4n4HFAiujNkWTztPxqOQ0Z12Oqw&s',
+    'https://www.fusionexus.com/wp-content/uploads/2018/11/IMG-fxlg.jpg',
+    'https://media.licdn.com/dms/image/C560BAQFOcJlUTD1hCw/company-logo_200_200/0/1630670422579/whalecloud_logo?e=2147483647&v=beta&t=UgKf0016oAmJfBPtu2nM6utCmTSmTuDfnBt5wpw1kaQ',
+    'https://www.fusionexus.com/wp-content/uploads/2018/11/IMG-fxlg.jpg'
+  ];
 
   onIntersection({ target, visible }: { target: Element; visible: boolean }): void {
 
