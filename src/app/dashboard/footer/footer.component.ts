83c424db9d014f86a5e7e92bcd4a60570be8e9f5
Sia Chee Zhen <cheezhensia24@gmail.com>
2024-05-09 17:32:53 +0800

    Added Footer.

--- src/app/dashboard/footer/footer.component.ts
+++ src/app/dashboard/footer/footer.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faFacebookSquare, faInstagram, faLinkedinIn, faHackerrank, faDev, faSteam, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { ScrollService } from '../shared/service/scroll.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  socials: any = [
+    { iconName: 'linkedin-in', iconClass: 'linkedin', iconRedirect: 'https://www.linkedin.com/in/Sia-WRWD/' },
+    { iconName: 'github', iconClass: 'github', iconRedirect: 'https://github.com/Sia-WRWD' },
+    { iconName: 'facebook-square', iconClass: 'facebook', iconRedirect: 'https://www.facebook.com/Sia.WRWD' },
+    { iconName: 'instagram', iconClass: 'instagram', iconRedirect: 'https://www.instagram.com/Sia-WRWD/' },
+    { iconName: 'hackerrank', iconClass: 'hackerrank', iconRedirect: 'https://www.hackerrank.com/profile/cheezhensia24' },
+    { iconName: 'dev', iconClass: 'dev', iconRedirect: 'https://devpost.com/Sia-WRWD' },
+    { iconName: 'steam', iconClass: 'steam', iconRedirect: 'https://steamcommunity.com/id/Sia-WRWD/' },
+    { iconName: 'discord', iconClass: 'discord', iconRedirect: 'https://www.discord.com/users/370570874438287363' }
+  ]
 
+  options: any = [
+    { name: "STACKS", anchor: "stacks", icon: "" },
+    { name: "WORK-EXP", anchor: "workexp", icon: "" },
+    { name: "PROJECTS", anchor: "project", icon: "" },
+    { name: "EDUCATION", anchor: "edu", icon: "" },
+    { name: "RESUME", anchor: "resume", icon: "" }
+  ]
+
+  constructor(library: FaIconLibrary, private el: ElementRef, private scrollService: ScrollService) {
+    library.addIcons(
+      faGithub,
+      faFacebookSquare,
+      faInstagram,
+      faLinkedinIn,
+      faHackerrank,
+      faDev,
+      faSteam,
+      faDiscord
+    )
+  }
+
+  scrollToSection(sectionId: string) {
+    this.scrollService.scrollToSection(sectionId);
+  }
 }
