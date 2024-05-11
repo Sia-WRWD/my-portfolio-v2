91257ea04b9648b6f406a58c62289b1e79a15d3a
Sia-WRWD <chinojen7@gmail.com>
2024-05-12 00:07:15 +0800

    Optimize Stack #21

--- src/app/dashboard/profile-content/profile-stacks/profile-stacks.component.ts
+++ src/app/dashboard/profile-content/profile-stacks/profile-stacks.component.ts
@@ -16,117 +16,66 @@ export class ProfileStacksComponent {
     {
       name: "Angular",
       link: "../../../assets/icons/stacks/angular.svg",
-      description: `My first ever JavaScript / TypeScript framework that I have mastered 
-          during my first internship, continue being utilized in my second internship 
-          and remained as one of my main framework aside from React's NextJS as this
-          framework offers better management capabilities for enterprise-level applications.`,
     },
     {
       name: "NextJS",
       link: "../../../assets/icons/stacks/next.svg",
-      description: `My second JavaScript / TypeScript framework that I have explored on
-          and is in the progress of mastering due to the fact that this framework being beginner-friendly
-          than the Angular framework (true but to a certain extend) and most important of all it offers 
-          more support in terms of animation libraries (such as Motion Framer) which Angular lacks.`,
     },
     {
       name: "SCSS",
       link: "../../../assets/icons/stacks/scss.svg",
-      description: `An extended version of CSS that has various different features that provides the css
-                code with more structure and readability which I have used a lot during both my internships, freelancing
-                and for assignments which led me to be very well-versed in this aspect.`,
     },
     {
       name: "Tailwind",
       link: "../../../assets/icons/stacks/tailwind.svg",
-      description: `A CSS framework that simplifies the process for coding the components css as it only 
-                requires defining the css in the HTML page of the component, reducing the sizes of css related files,
-                making it very efficient and time-saving.`,
     },
     {
       name: "Java",
       link: "../../../assets/icons/stacks/java.svg",
-      description: `A high-level, class-based OOP language that I have utilized for developing various different
-                type of applications that include Enterprise-Level application (Java EE), Desktop Applications (Swing), 
-                and Backend Development (Java Spring Boot).`,
     },
     {
       name: "Firebase",
       link: "../../../assets/icons/stacks/firebase.svg",
-      description: `A great access to cloud services by Google that is integral to my portfolio, offering 
-                essential tools for NoSQL Cloud Database storage, application hosting, deployment, and streamlined user 
-                authentication, facilitating seamless project development and management.`,
     },
     {
       name: "GitHub",
       link: "https://cdn.worldvectorlogo.com/logos/github-icon-1.svg",
-      description: `GitHub stands as a cornerstone in my portfolio, serving as the hub where I store, manage,
-                and showcase my code. It's an essential tool woven into both my professional ventures and personal projects, 
-                elevating them with meticulously crafted README.md files that captivate and inform.`,
     },
     {
       name: "Python",
       link: "../../../assets/icons/stacks/python.svg",
-      description: `Despite Python's reputation as a highly versatile language suitable for a wide range of applications, 
-                I have only been utilizing it in developing AI models and APIs. This preference is largely influenced by the 
-                vibrant community, which consistently introduces new libraries and updates, enhancing Python's capabilities.`,
     },
     {
       name: "TensorFlow",
       link: "../../../assets/icons/stacks/tensorflow.svg",
-      description: `A free and open-source software library for machine learning and artificial intelligence, which I have
-                personally utilized in building Deep Learning models and explored on the many different pre-trained models' codes
-                offered in this library.`,
     },
     {
       name: "NodeJS",
       link: "../../../assets/icons/stacks/nodejs.svg",
-      description: `Node.js is a cross-platform, open-source JavaScript runtime environment which I have utilized in
-                creating the back-end for web applications, and in the context of AI Model Development, it is more used on
-                creating an API for the model to be callable from the front-end.`,
     },
     {
       name: "Azure Functions",
       link: "../../../assets/icons/stacks/azure.svg",
-      description: `An event-driven, serverless compute platform that I've used a serverless compute platform for the 
-                ClassInsight project's transcribe video API. It's triggered on demand, making it efficient, but it can only 
-                handle videos up to 20 minutes long due to the limitations imposed.`,
     },
     {
       name: "Azure OpenAI",
       link: "../../../assets/icons/stacks/openai.svg",
-      description: `A REST API which includes language models such as GPT-3 and GPT-4, I've seamlessly integrated it 
-                into the ClassInsight project, facilitating the summarization of extensive two-hour lectures into concise, 
-                digestible content, condensing key points into a five-minute read, enhancing learning efficiency.`,
     },
     {
       name: "MySQL",
       link: "../../../assets/icons/stacks/mysql.svg",
-      description: `My first Relational DBMS and have continued to use it in both work and hobby projects
-                alike. It is powerful compared to some NoSQL databases due to unique functionalities offered, such as
-                stored procedures, and etc.
-                `,
     },
     {
       name: "Firebase",
       link: "../../../assets/icons/stacks/firebase.svg",
-      description: `A great access to cloud services by Google that is integral to my portfolio, offering 
-                essential tools for NoSQL Cloud Database storage, application hosting, deployment, and streamlined user 
-                authentication, facilitating seamless project development and management.`,
     },
     {
       name: "Azure CosmoDB",
       link: "../../../assets/icons/stacks/cosmodb.png",
-      description: `Can't really say I am very well-versed with this DBMS as I have only utilized a handful of
-                times, mostly for Azure Related Hackathons. Even so, one advantage I can see when compared to traditional
-                databases are the ability to scale and efficient global distribution.`,
     },
     {
       name: "MongoDB",
       link: "../../../assets/icons/stacks/mongodb.svg",
-      description: `Another NoSQL DBMS that I have utilized in conjunction with Angular and React frameworks for
-                personal projects and work. But, compared to MySQL, this is a bit lesser and though the case, from using it,
-                I can see its viability in terms of being document-oriented (JSON-like), making it easy for handling.`,
     },
   ]
   animationPlayed: boolean = false;
