3af097748a38bf2a3209cab4ba65cf7323a682e0
Sia-WRWD <chinojen7@gmail.com>
2024-05-12 00:10:10 +0800

    Changed Position

--- src/app/dashboard/shared/data/stack-data.ts
+++ src/app/dashboard/shared/data/stack-data.ts
@@ -6,8 +6,6 @@ export const stacks: any = [
           during my first internship, continue being utilized in my second internship 
           and remained as one of my main framework aside from React's NextJS as this
           framework offers better management capabilities for enterprise-level applications.`,
-        offset: 40,
-        opacity: 'selected'
     },
     {
         name: "NextJS",
@@ -16,8 +14,6 @@ export const stacks: any = [
           and is in the progress of mastering due to the fact that this framework being beginner-friendly
           than the Angular framework (true but to a certain extend) and most important of all it offers 
           more support in terms of animation libraries (such as Motion Framer) which Angular lacks.`,
-        offset: 55,
-        opacity: 'selected'
     },
     {
         name: "SCSS",
@@ -25,8 +21,6 @@ export const stacks: any = [
         description: `An extended version of CSS that has various different features that provides the css
                 code with more structure and readability which I have used a lot during both my internships, freelancing
                 and for assignments which led me to be very well-versed in this aspect.`,
-        offset: 35,
-        opacity: 'selected'
     },
     {
         name: "Tailwind",
@@ -34,8 +28,6 @@ export const stacks: any = [
         description: `A CSS framework that simplifies the process for coding the components css as it only 
                 requires defining the css in the HTML page of the component, reducing the sizes of css related files,
                 making it very efficient and time-saving.`,
-        offset: 30,
-        opacity: 'selected'
     },
     {
         name: "Java",
@@ -43,8 +35,6 @@ export const stacks: any = [
         description: `A high-level, class-based OOP language that I have utilized for developing various different
                 type of applications that include Enterprise-Level application (Java EE), Desktop Applications (Swing), 
                 and Backend Development (Java Spring Boot).`,
-        offset: 30,
-        opacity: 'selected'
     },
     {
         name: "Firebase",
@@ -52,8 +42,6 @@ export const stacks: any = [
         description: `A great access to cloud services by Google that is integral to my portfolio, offering 
                 essential tools for NoSQL Cloud Database storage, application hosting, deployment, and streamlined user 
                 authentication, facilitating seamless project development and management.`,
-        offset: 35,
-        opacity: 'selected'
     },
     {
         name: "GitHub",
@@ -61,8 +49,6 @@ export const stacks: any = [
         description: `GitHub stands as a cornerstone in my portfolio, serving as the hub where I store, manage,
                 and showcase my code. It's an essential tool woven into both my professional ventures and personal projects, 
                 elevating them with meticulously crafted README.md files that captivate and inform.`,
-        offset: 40,
-        opacity: 'selected'
     },
     {
         name: "Python",
@@ -70,8 +56,6 @@ export const stacks: any = [
         description: `Despite Python's reputation as a highly versatile language suitable for a wide range of applications, 
                 I have only been utilizing it in developing AI models and APIs. This preference is largely influenced by the 
                 vibrant community, which consistently introduces new libraries and updates, enhancing Python's capabilities.`,
-        offset: 90,
-        opacity: 'selected'
     },
     {
         name: "TensorFlow",
@@ -79,7 +63,6 @@ export const stacks: any = [
         description: `A free and open-source software library for machine learning and artificial intelligence, which I have
                 personally utilized in building Deep Learning models and explored on the many different pre-trained models' codes
                 offered in this library.`,
-        opacity: 'selected'
     },
     {
         name: "NodeJS",
@@ -87,7 +70,6 @@ export const stacks: any = [
         description: `Node.js is a cross-platform, open-source JavaScript runtime environment which I have utilized in
                 creating the back-end for web applications, and in the context of AI Model Development, it is more used on
                 creating an API for the model to be callable from the front-end.`,
-        opacity: 'selected'
     },
     {
         name: "Azure Functions",
@@ -95,7 +77,6 @@ export const stacks: any = [
         description: `An event-driven, serverless compute platform that I've used a serverless compute platform for the 
                 ClassInsight project's transcribe video API. It's triggered on demand, making it efficient, but it can only 
                 handle videos up to 20 minutes long due to the limitations imposed.`,
-        opacity: 'selected'
     },
     {
         name: "Azure OpenAI",
@@ -103,7 +84,6 @@ export const stacks: any = [
         description: `A REST API which includes language models such as GPT-3 and GPT-4, I've seamlessly integrated it 
                 into the ClassInsight project, facilitating the summarization of extensive two-hour lectures into concise, 
                 digestible content, condensing key points into a five-minute read, enhancing learning efficiency.`,
-        opacity: 'selected'
     },
     {
         name: "MySQL",
@@ -112,7 +92,6 @@ export const stacks: any = [
                 alike. It is powerful compared to some NoSQL databases due to unique functionalities offered, such as
                 stored procedures, and etc.
                 `,
-        opacity: 'selected'
     },
     {
         name: "Firebase",
@@ -120,7 +99,6 @@ export const stacks: any = [
         description: `A great access to cloud services by Google that is integral to my portfolio, offering 
                 essential tools for NoSQL Cloud Database storage, application hosting, deployment, and streamlined user 
                 authentication, facilitating seamless project development and management.`,
-        opacity: 'selected'
     },
     {
         name: "Azure CosmoDB",
@@ -128,7 +106,6 @@ export const stacks: any = [
         description: `Can't really say I am very well-versed with this DBMS as I have only utilized a handful of
                 times, mostly for Azure Related Hackathons. Even so, one advantage I can see when compared to traditional
                 databases are the ability to scale and efficient global distribution.`,
-        opacity: 'selected'
     },
     {
         name: "MongoDB",
@@ -136,6 +113,5 @@ export const stacks: any = [
         description: `Another NoSQL DBMS that I have utilized in conjunction with Angular and React frameworks for
                 personal projects and work. But, compared to MySQL, this is a bit lesser and though the case, from using it,
                 I can see its viability in terms of being document-oriented (JSON-like), making it easy for handling.`,
-        opacity: 'selected'
     },
 ]
